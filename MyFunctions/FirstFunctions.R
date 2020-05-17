8b0b01691d8407aefe3bce0c6ae132ca1b54b92f
Mauricio Amarante <mauricio.amarante@gmail.com>
2020-05-17 13:01:56 -0300

    Comments added to the function

--- MyFunctions/FirstFunctions.R
+++ MyFunctions/FirstFunctions.R
@@ -2,22 +2,25 @@ add2 <- function(x, y) {
         x+y
 }
 above10 <- function(x) {
+        ## x is a vector and function will return the numbers above 10
         use <- x > 10
         x[use]
 }
 
 above <- function(x, n = 10) {
+      ## x is a vector and function will return the numbers above n, default 10
       use <- x > n
       x[use]
 }
 
 columnmean <- function(y, removeNA = TRUE) {
-     nc <- ncol(y)
-     means <- numeric(nc)
-     for(i in 1:nc) {
+    ## y is a Matrix or Data Frame 
+    nc <- ncol(y)
+    means <- numeric(nc)
+    for(i in 1:nc) {
           means[i] <- mean(y[,i], na.rm = removeNA)
-     }
-     means
+    }
+    means
 }
 
 
